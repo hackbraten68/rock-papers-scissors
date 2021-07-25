6820f95c4a50a5b579333d5ef34be5362788a24f
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2021-07-25 21:56:41 +0200

    basic main markup

---
+++ index.js
@@ -0,0 +1,31 @@
+let signHand = ["rock", "paper", "scissors"];
+const startNewGame = 0
+const userRock = document.getElementById("rock")
+const userPaper = document.getElementById("paper")
+const userScissors = document.getElementById("scissors")
+
+
+
+// start the game
+
+// 3 buttons für user
+// 
+// user button rock:
+userRock.addEventListener("click", function(_rock) {
+    console.log("button clicked!")
+})
+
+userPaper.addEventListener("click", function(_paper) {
+    console.log("button clicked!")
+})
+
+userScissors.addEventListener("click", function(_scissors) {
+    console.log("button clicked!")
+})
+
+// computer generiert random move
+// wer gewinnt die runde ?
+// counter score wer vorne liegt
+// nach 3 runden wer gewinnt das game
+
+
