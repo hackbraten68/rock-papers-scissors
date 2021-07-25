7b617d6620fcfe67ee53a20363f60cc38d893c19
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2021-07-25 22:58:42 +0200

    Update index.js
    far away from clean code, but logic works so far

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let signHand = ["rock", "paper", "scissors"];
+let userMove = 0
+let botRandom = Math.floor(Math.random() * 3)
 const startNewGame = 0
 const userRock = document.getElementById("rock")
 const userPaper = document.getElementById("paper")
@@ -12,25 +14,74 @@ const userScissors = document.getElementById("scissors")
 // 
 // user button rock:
 userRock.addEventListener("click", function() {
-    console.log("button clicked!")
+    userMove = 0
+    console.log("You choose: ROCK!")
+    botRandomMove()
+    console.log(botRandomMove())
+    whoWonRound()
+    console.log(whoWonRound())
 })
 // user button paper:
 userPaper.addEventListener("click", function() {
-    console.log("button clicked!")
+    userMove = 1
+    console.log("You choose: PAPER!")
+    botRandomMove()
+    console.log(botRandomMove())
+    whoWonRound()
+    console.log(whoWonRound())
 })
 // user button scissors:
 userScissors.addEventListener("click", function() {
-    console.log("button clicked!")
+    userMove = 2
+    console.log("You choose: SCISSORS!")
+    botRandomMove()
+    console.log(botRandomMove())
+    whoWonRound()
+    console.log(whoWonRound())
 })
 
 // computer generiert random move
 // 
 function botRandomMove() {
-    return Math.floor(Math.random() * 3)
+    
+        if (botRandom === 0) {
+            return "Bot choose: ROCK!"
+        }   else if (botRandom === 1) {
+            return "Bot choose: PAPER!"
+        }   else if (botRandom === 2) {
+            return "Bot choose: SCISSORS!"
+        }
 }
 
-console.log(botRandomMove())
+
 // wer gewinnt die runde ?
+// 
+function whoWonRound() {
+    // USER CHOOSE ROCK
+    if (userMove === 0 && botRandom === 2) {
+        return "ROCK beats SCISSORS"
+    } else if (userMove === 0 && botRandom === 1) {
+        return "PAPER beats ROCK"
+    } else if (userMove === botRandom) {
+        return "TIE GAME !"
+    }
+    // USER CHOOSE PAPER
+    if (userMove === 1 && botRandom === 0) {
+        return "PAPER beats ROCK"
+    } else if (userMove === 1 && botRandom === 2) {
+        return "SCISSORS beats PAPER"
+    } else if (userMove === botRandom) {
+        return "TIE GAME !"
+    }
+    // USER CHOOSE SCISSORS
+    if (userMove === 2 && botRandom === 1) {
+        return "SCISSORS beats PAPER"
+    } else if (userMove === 2 && botRandom === 0) {
+        return "ROCK beats SCISSORS"
+    } else if (userMove === botRandom) {
+        return "TIE GAME !"
+    }
+}
 // counter score wer vorne liegt
 // nach 3 runden wer gewinnt das game
 
